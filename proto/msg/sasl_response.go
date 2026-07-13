f96b7a0703462e3a2fe4eb49d5fc85c8249b5416
Joshua Jones <joshua.jones.software@gmail.com>
2026-07-12 22:19:21 -0400

    update

---
+++ proto/msg/sasl_response.go
@@ -0,0 +1,43 @@
+package msg
+
+import (
+	"gopsql/internal/bytex"
+	"math"
+)
+
+const KindSASLResponse byte = 'p'
+
+var _ Message = &SASLResponse{}
+var _ Frontend = &SASLResponse{}
+
+type SASLResponse struct {
+	Data []byte
+}
+
+func (x *SASLResponse) message() {}
+
+func (x *SASLResponse) frontend() {}
+
+func (x *SASLResponse) AppendBinary(b []byte) ([]byte, error) {
+	sizeData := len(x.Data)
+
+	length := sizeMessageLength + sizeData
+
+	if length > math.MaxInt32 {
+		return b, invalidFormat(bytex.ErrValueOverflow)
+	}
+
+	size := sizeMessageKind + length
+
+	buf := bytex.NewBuffer(b)
+
+	buf.Grow(size)
+	buf.AppendByte(KindSASLResponse)
+	buf.AppendInt32(int32(length))
+	buf.AppendByte(x.Data...)
+	return buf.Bytes(), nil
+}
+
+func (x *SASLResponse) UnmarshalBinary(b []byte) error {
+
+}
